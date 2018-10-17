b6a9f4f3f297c971440b3ccb79c1d599ce679065
Robert Baxter <roer.baxter@gmail.com>
2018-10-17 16:59:02 -0400

    heroku client-side deployment

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -4,8 +4,8 @@ switch (window.location.hostname) {
   case "localhost" || "127.0.0.1":
     APIURL = "http://localhost:4000";
     break;
-  case "fastback-mobile-feedback.herokuapp.com/":
-    APIURL = "https://fastback-mobile-server.herokuapp.com/";
+  case "fastback-mobile-feedback.herokuapp.com":
+    APIURL = "https://fastback-mobile-server.herokuapp.com";
     break;
   default:
     console.log("Error in environment.js");
