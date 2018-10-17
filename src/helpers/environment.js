153d632e3f82d2a58b8c2ec54fa90bb49b4b9fac
Robert Baxter <roer.baxter@gmail.com>
2018-10-17 16:54:39 -0400

    heroku client-side deployment

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -4,7 +4,7 @@ switch (window.location.hostname) {
   case "localhost" || "127.0.0.1":
     APIURL = "http://localhost:4000";
     break;
-  case "https://fastback-mobile-client.herokuapp.com/":
+  case "fastback-mobile-feedback.herokuapp.com/":
     APIURL = "https://fastback-mobile-server.herokuapp.com/";
     break;
   default:
